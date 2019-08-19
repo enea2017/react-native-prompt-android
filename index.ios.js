da612d6117a334979cb92213d67a23c6e0650bc1
enea2017 <enea.dume2017@gmail.com>
2019-08-19 19:35:02 +0800

    [update] Replace AlertIOS with Alert

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,12 @@
 import {
-    AlertIOS
+    Alert
 } from 'react-native';
-
+    
 export default function prompt(
     title: ?string,
     message?: ?string,
     callbackOrButtons?: ?((text: string) => void) | Object,
     options?: Object
 ): void {
-    AlertIOS.prompt(title, message, callbackOrButtons, options.type, options.defaultValue, options.keyboardType);
+    Alert.prompt(title, message, callbackOrButtons, options.type, options.defaultValue, options.keyboardType);
 };
